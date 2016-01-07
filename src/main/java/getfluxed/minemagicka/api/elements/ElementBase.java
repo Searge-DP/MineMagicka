abeb1fe44d22245515a0962e3364951d7785ce96
yrsegal <yrsegal@gmail.com>
2016-01-07 11:48:52 -0500

    element colorization

--- src/main/java/getfluxed/minemagicka/api/elements/ElementBase.java
+++ src/main/java/getfluxed/minemagicka/api/elements/ElementBase.java
@@ -4,13 +4,18 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import org.lwjgl.opengl.GL11;
 
+import java.awt.*;
+
 public abstract class ElementBase implements IElement {
 
     @SuppressWarnings("static-access")
     public void render(Gui gui, int x, int y) {
         GL11.glPushMatrix();
+        Color color = new Color(this.getColor());
+        GL11.glColor3f(color.getRed()/255f, color.getGreen()/255f, color.getBlue()/255f);
         Minecraft.getMinecraft().getTextureManager().bindTexture(getTexture());
         gui.func_152125_a(x, y, 0, 0, 24, 24, 24, 24, 24, 24);
+        GL11.glColor3f(1f, 1f, 1f);
         GL11.glPopMatrix();
     }
 
