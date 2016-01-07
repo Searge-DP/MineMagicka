1dbb0ef7c565ef5222eaaa96b5e14ae824ff8ef0
yrsegal <yrsegal@gmail.com>
2016-01-06 20:01:43 -0500

    add a todo in the Chapter class

--- src/main/java/getfluxed/minemagicka/client/gui/book/Chapter.java
+++ src/main/java/getfluxed/minemagicka/client/gui/book/Chapter.java
@@ -1,6 +1,6 @@
 package getfluxed.minemagicka.client.gui.book;
 
-public class Chapter {
+public class Chapter { // TODO: 1/6/16 Implement getfluxed.minemagicka.api.compendium.ICompendiumChapter 
 
     private String name;
     private String unLocalizedName;
