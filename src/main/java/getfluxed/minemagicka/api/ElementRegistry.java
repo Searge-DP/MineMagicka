322d653a0d7bb160ef45f08fa71f2a09c3eb93c5
Jared <jaredlll08@gmail.com>
2016-01-11 13:14:28 +0200

    Made ElementRegistry.elements a LinkedHashMap, so elements can be displayed better
    Having shield as the first element is a bit weird...

--- src/main/java/getfluxed/minemagicka/api/ElementRegistry.java
+++ src/main/java/getfluxed/minemagicka/api/ElementRegistry.java
@@ -1,13 +1,13 @@
 package getfluxed.minemagicka.api;
 
-import getfluxed.minemagicka.api.elements.IElement;
-
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import getfluxed.minemagicka.api.elements.IElement;
 
 public class ElementRegistry {
 
-    private static Map<String, IElement> elements = new HashMap<String, IElement>();
+    private static Map<String, IElement> elements = new LinkedHashMap<String, IElement>();
 
     public static void registerElement(String key, IElement element) {
         elements.put(key, element);
