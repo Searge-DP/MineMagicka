388a1ebebbe4edafac15851e26cd99581d0ae894
yrsegal <yrsegal@gmail.com>
2016-01-21 22:07:49 -0500

    polish radial GUI slightly
    i’ll work on getting the things to render later

--- src/main/java/getfluxed/minemagicka/client/render/radial/RadialGUIHandler.java
+++ src/main/java/getfluxed/minemagicka/client/render/radial/RadialGUIHandler.java
@@ -79,6 +79,8 @@ public class RadialGUIHandler {
     public void onRender(RenderGameOverlayEvent.Pre e) {
         if (e.type == RenderGameOverlayEvent.ElementType.CROSSHAIRS && getGuiState()) {
 
+            final float transparency = 0.7f;
+
             int sections = getActiveGUI().size();
             int outerRad = 360;
             if (sections > 0) outerRad /= sections;
@@ -92,29 +94,35 @@ public class RadialGUIHandler {
             float y1 = e.resolution.getScaledHeight()/2;
             float x2;
             float y2;
+            float x3;
+            float y3;
 
             GlStateManager.enableBlend();
             GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            GlStateManager.color(0.0f,0.0f,0.0f,0.5f);
+            GlStateManager.color(0.0f,0.0f,0.0f,transparency);
 
             GL11.glBegin(GL11.GL_TRIANGLE_FAN);
-            GL11.glVertex2f(x1,y1);
+
 
             int slice;
             int oldSlice = 0;
 
-            for (float angle=0f; angle <= 360; angle++) {
-                slice = (int) angle/(outerRad+1);
-                if (slice != oldSlice) {
+            GL11.glVertex2f(x1, y1);
+
+            for (int angle = 0; angle <= 360; angle++) {
+                slice = Math.round((angle)/(outerRad+0.5f));
+                if (slice != oldSlice && angle != 360) {
                     oldSlice = slice;
                     if (slice % 2 == 0)
-                        GlStateManager.color(0.0f,0.0f,0.0f,0.5f);
+                        GlStateManager.color(0.0f,0.0f,0.0f,transparency);
                     else
-                        GlStateManager.color(0.5f,0.5f,0.5f,0.5f);
+                        GlStateManager.color(0.5f,0.5f,0.5f,transparency);
                 }
-                x2 = x1 + MathHelper.sin((float) (angle * Math.PI/180)) * radius;
-                y2 = y1 + MathHelper.cos((float) (angle * Math.PI/180)) * radius;
-                GL11.glVertex2f(x2,y2);
+                x2 = MathHelper.sin((float) (angle * Math.PI/180)) * radius;
+                y2 = MathHelper.cos((float) (angle * Math.PI/180)) * radius;
+                x3 = x1+x2;
+                y3 = y1+y2;
+                GL11.glVertex2f(x3,y3);
             }
 
             GlStateManager.color(1f,1f,1f,1f);
